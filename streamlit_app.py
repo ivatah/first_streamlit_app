b4398b0b136970db42159b13d89b2d59d2dca5a9
ivatah <99807597+ivatah@users.noreply.github.com>
2023-08-25 15:27:37 -0300

    Update streamlit_app.py
    Added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfest Menu')
 streamlit.text('Omega 3')
 streamlit.text('Kale')
 streamlit.text('Hard Boiled')
