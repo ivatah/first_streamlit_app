478f4207eb55d0298652f86268b981ed917bbc5f
ivatah <99807597+ivatah@users.noreply.github.com>
2023-08-25 15:34:39 -0300

    Update streamlit_app.py
    fixed texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfest Menu')
-streamlit.text('Omega 3')
-streamlit.text('Kale')
-streamlit.text('Hard Boiled')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
